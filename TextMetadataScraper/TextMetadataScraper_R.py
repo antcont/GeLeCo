5112e783325b72a669ade31826f06fb472226964
antcont <antoniogiovannicontarino@gmail.com>
2021-03-17 10:24:27 +0100

    removed yaspin

--- TextMetadataScraper/TextMetadataScraper_R.py
+++ TextMetadataScraper/TextMetadataScraper_R.py
@@ -18,7 +18,6 @@ from bs4 import BeautifulSoup
 from urllib3.util import Retry
 import urllib3
 import re
-from yaspin import yaspin
 from xml.sax.saxutils import escape, unescape
 
 
@@ -33,133 +32,127 @@ with open(path_input, "r") as f:
     mylist = f.read().splitlines()
 
 len_list = len(mylist)
-
 corpus_as_list = []
 
-print()
-with yaspin().bold.cyan.aesthetic as sp:  # printing spinner and % progress
-    for id, url in enumerate(mylist):
-        html = http.request('GET', url).data
-        soup = BeautifulSoup(html, features="lxml")
+for id, url in enumerate(mylist):
+    html = http.request('GET', url).data
+    soup = BeautifulSoup(html, features="lxml")
 
-        # getting/setting metadata
-        type = "Gerichtsentscheidung"
-        level = "Bund"
-        database_URL = "rechtsprechung-im-internet.de"
-        title_abbreviation = "NA"
+    # getting/setting metadata
+    type = "Gerichtsentscheidung"
+    level = "Bund"
+    database_URL = "rechtsprechung-im-internet.de"
+    title_abbreviation = "NA"
 
-        # getting title
-        title_find = soup.find(class_="RspDL")
-        if title_find:
-            title = title_find.get_text(strip=True)
-            title = escape(unescape(title))
-            title = title.replace("\"", "&quot;")
-            if "\n" in title:
-                # to avoid newlines that would mess the XML tag; this happens when the court decision has no defined
-                # title and the first sentence of the decision is caught by the script as "title"
-                title = "NA"
-            match = re.match(r"\d[A-ZÖÄÜ]", title) # if decision title begins with "1D" (es "1Der Kläger..."), set as NA
-            if match:
-                title = "NA"
-        else:
+    # getting title
+    title_find = soup.find(class_="RspDL")
+    if title_find:
+        title = title_find.get_text(strip=True)
+        title = escape(unescape(title))
+        title = title.replace("\"", "&quot;")
+        if "\n" in title:
+            # to avoid newlines that would mess the XML tag; this happens when the court decision has no defined
+            # title and the first sentence of the decision is caught by the script as "title"
             title = "NA"
+        match = re.match(r"\d[A-ZÖÄÜ]", title) # if decision title begins with "1D" (es "1Der Kläger..."), set as NA
+        if match:
+            title = "NA"
+    else:
+        title = "NA"
 
-        # getting drafting date
-        drafting_date_match = re.search(r"Entscheidungsdatum:(.{8,10})", soup.get_text(strip=True))
-        if drafting_date_match:
-            drafting_date = drafting_date_match.group(1)
-        else:
-            drafting_date = "NA"
+    # getting drafting date
+    drafting_date_match = re.search(r"Entscheidungsdatum:(.{8,10})", soup.get_text(strip=True))
+    if drafting_date_match:
+        drafting_date = drafting_date_match.group(1)
+    else:
+        drafting_date = "NA"
 
-        # getting year
-        year_match = re.search(r".+ ?(\d{4}$)", drafting_date)
-        if year_match:
-            year = year_match.group(1)
-        else:
-            year = "NA"
+    # getting year
+    year_match = re.search(r".+ ?(\d{4}$)", drafting_date)
+    if year_match:
+        year = year_match.group(1)
+    else:
+        year = "NA"
 
-        # getting decade by slicing 7t to 9th character of drafting date and adding 0
-        if drafting_date == "NA":
-            decade = "NA"
-        else:
-            decade = drafting_date[6:9] + "0"
+    # getting decade by slicing 7t to 9th character of drafting date and adding 0
+    if drafting_date == "NA":
+        decade = "NA"
+    else:
+        decade = drafting_date[6:9] + "0"
 
-        # getting court detail
-        court_detail_match = soup.find(string="Gericht:")
-        if court_detail_match:
-            court_detail = court_detail_match.next_element.get_text(strip=True)
-            if "\n" in court_detail or len(court_detail) > 30:
-                court_detail = "NA"
-        else:
+    # getting court detail
+    court_detail_match = soup.find(string="Gericht:")
+    if court_detail_match:
+        court_detail = court_detail_match.next_element.get_text(strip=True)
+        if "\n" in court_detail or len(court_detail) > 30:
             court_detail = "NA"
+    else:
+        court_detail = "NA"
 
-        # getting court from court detail
-        if court_detail == "NA":
+    # getting court from court detail
+    if court_detail == "NA":
+        court = "NA"
+    else:
+        court_match = re.search(r"^(\w+) ", court_detail)
+        if court_match:
+            court = court_match.group(1)
+        else:
             court = "NA"
-        else:
-            court_match = re.search(r"^(\w+) ", court_detail)
-            if court_match:
-                court = court_match.group(1)
-            else:
-                court = "NA"
 
-        # getting reference
-        reference_match = soup.find(string="Aktenzeichen:")
-        if reference_match:
-            reference = reference_match.next_element.get_text(strip=True)
-        else:
-            reference = "NA"
+    # getting reference
+    reference_match = soup.find(string="Aktenzeichen:")
+    if reference_match:
+        reference = reference_match.next_element.get_text(strip=True)
+    else:
+        reference = "NA"
 
-        # getting ECLI
-        ECLI_match = soup.find(string="ECLI:")
-        if ECLI_match:
-            ECLI = ECLI_match.next_element.get_text(strip=True)
-            if "\n" in ECLI:
-                ECLI = "NA"
-        else:
+    # getting ECLI
+    ECLI_match = soup.find(string="ECLI:")
+    if ECLI_match:
+        ECLI = ECLI_match.next_element.get_text(strip=True)
+        if "\n" in ECLI:
             ECLI = "NA"
+    else:
+        ECLI = "NA"
 
-        # getting decision_type (Dokumenttyp)
-        decisiontype_match = soup.find(string="Dokumenttyp:")
-        if decisiontype_match:
-            decision_type = decisiontype_match.next_element.get_text(strip=True)
-            if "\n" in decision_type:
-                decision_type = "NA"
-        else:
+    # getting decision_type (Dokumenttyp)
+    decisiontype_match = soup.find(string="Dokumenttyp:")
+    if decisiontype_match:
+        decision_type = decisiontype_match.next_element.get_text(strip=True)
+        if "\n" in decision_type:
             decision_type = "NA"
+    else:
+        decision_type = "NA"
 
-        # building the <text> tag
-        text_tag = '<text type="%s" level="%s" title="%s" title_abbreviation="%s" drafting_date="%s" decade="%s"' \
-                   ' database_URL="%s" court="%s" court_detail="%s" reference="%s" year="%s" decision_type="%s"' \
-                   ' ECLI="%s">' \
-                   % (type, level, title, title_abbreviation, drafting_date, decade, database_URL, court,
-                      court_detail, reference, year, decision_type, ECLI)
+    # building the <text> tag
+    text_tag = '<text type="%s" level="%s" title="%s" title_abbreviation="%s" drafting_date="%s" decade="%s"' \
+               ' database_URL="%s" court="%s" court_detail="%s" reference="%s" year="%s" decision_type="%s"' \
+               ' ECLI="%s">' \
+               % (type, level, title, title_abbreviation, drafting_date, decade, database_URL, court,
+                  court_detail, reference, year, decision_type, ECLI)
 
-        # scraping and adding the text body
-        body = soup.get_text('\n', strip=True)
+    # scraping and adding the text body
+    body = soup.get_text('\n', strip=True)
 
-        # checking whether scraper has correctly navigated to the right URL or webpage does not contain any decision
-        no_decision = "Das Bundesministerium der Justiz und für Verbraucherschutz und das Bundesamt für Justiz " \
-                      "stellen für interessierte Bürgerinnen und Bürger ausgewählte Entscheidungen des" \
-                      " Bundesverfassungsgerichts, der obersten Gerichtshöfe des Bundes sowie des" \
-                      " Bundespatentgerichts ab dem Jahr 2010 kostenlos im Internet bereit."
-        if no_decision in body:
-            # discarding, exiting current loop and scraping next URL
-            continue
+    # checking whether scraper has correctly navigated to the right URL or webpage does not contain any decision
+    no_decision = "Das Bundesministerium der Justiz und für Verbraucherschutz und das Bundesamt für Justiz " \
+                  "stellen für interessierte Bürgerinnen und Bürger ausgewählte Entscheidungen des" \
+                  " Bundesverfassungsgerichts, der obersten Gerichtshöfe des Bundes sowie des" \
+                  " Bundespatentgerichts ab dem Jahr 2010 kostenlos im Internet bereit."
+    if no_decision in body:
+        # discarding, exiting current loop and scraping next URL
+        continue
 
+    # adding <text> tag, body and closing tag
+    corpus_as_list.append(text_tag)
+    corpus_as_list.append(body)
+    corpus_as_list.append("</text>")
 
-        # adding <text> tag, body and closing tag
-        corpus_as_list.append(text_tag)
-        corpus_as_list.append(body)
-        corpus_as_list.append("</text>")
-
-        # printing progress
-        sp.text = "   %i out of %i scraped (%.2f%%)" % (id, len_list, (id/len_list*100))
-
-corpus_as_string = "\n".join(corpus_as_list)
+    # printing progress
+    print("\r", "%i out of %i scraped (%.2f%%)" % (id, len_list, (id/len_list*100)))
 
 with open(path_output, "w+", encoding="utf-8") as file:
-    file.write(corpus_as_string)
+    file.write("\n".join(corpus_as_list))
 
 print("Done.")
 
--- TextMetadataScraper/TextMetadataScraper_V.py
+++ TextMetadataScraper/TextMetadataScraper_V.py
@@ -18,8 +18,6 @@ from bs4 import BeautifulSoup
 from urllib3.util import Retry
 import urllib3
 import re
-from yaspin import yaspin
-from yaspin.spinners import Spinners
 from xml.sax.saxutils import escape, unescape
 
 retries = Retry(connect=5, read=2, redirect=5)
@@ -34,69 +32,68 @@ with open(path_input, "r") as f:
 
 corpus_as_list = []
 
-with yaspin(Spinners.aesthetic) as sp:  # printing spinner and % progress
-    for id, url in enumerate(mylist):
-        html = http.request('GET', url).data
-        soup = BeautifulSoup(html, features="lxml")
+for id, url in enumerate(mylist):
+    html = http.request('GET', url).data
+    soup = BeautifulSoup(html, features="lxml")
 
-        # getting/setting metadata
-        type = "Verwaltungsvorschrift"           #these must be changed for each subcorpus
-        level = "Bund"
-        database_URL = "verwaltungsvorschriften-im-internet.de"
-        court = "NA"
-        court_detail = "NA"
-        reference = "NA"
-        decision_type = "NA"
-        ECLI = "NA"
+    # getting/setting metadata
+    type = "Verwaltungsvorschrift"           #these must be changed for each subcorpus
+    level = "Bund"
+    database_URL = "verwaltungsvorschriften-im-internet.de"
+    court = "NA"
+    court_detail = "NA"
+    reference = "NA"
+    decision_type = "NA"
+    ECLI = "NA"
 
-        # getting title and abbreviation (if any)
-        titlex = soup.find("title").get_text(strip=True)
-        # abbreviation can be found at the end of the title, between parentheses, sometimes followed by the drafting date
-        title_match = re.search(r"^(.+) ?\((.+)\)( (vom|v\.) (\d\d?\.\d\d?\.\d\d\d\d|\d\d?\. \w{3,10} \d\d\d\d))? ?$", titlex)
-        if title_match: # matching titles with final parenthesis containing the abbreviation
-            title = title_match.group(1)
-            title_abbreviation = title_match.group(2)
-            title_abbreviation = escape(unescape(title_abbreviation))
-            title_abbreviation = title_abbreviation.replace("\"", "&quot;")
-        else: #if title has not a final parenthesis with the abbreviation
-            title = titlex
-            title_abbreviation = "NA"
-        title = escape(unescape(title))
-        title = title.replace("\"", "&quot;")
+    # getting title and abbreviation (if any)
+    titlex = soup.find("title").get_text(strip=True)
+    # abbreviation can be found at the end of the title, between parentheses, sometimes followed by the drafting date
+    title_match = re.search(r"^(.+) ?\((.+)\)( (vom|v\.) (\d\d?\.\d\d?\.\d\d\d\d|\d\d?\. \w{3,10} \d\d\d\d))? ?$", titlex)
+    if title_match: # matching titles with final parenthesis containing the abbreviation
+        title = title_match.group(1)
+        title_abbreviation = title_match.group(2)
+        title_abbreviation = escape(unescape(title_abbreviation))
+        title_abbreviation = title_abbreviation.replace("\"", "&quot;")
+    else: #if title has not a final parenthesis with the abbreviation
+        title = titlex
+        title_abbreviation = "NA"
+    title = escape(unescape(title))
+    title = title.replace("\"", "&quot;")
 
 
-        #getting drafting_date from the title. unfortunately, there is no other specific section where the drafting_date is to be found coherently
-        match_date = re.search(r"^.+(vom|v\.) (\d\d?\.\d\d?\.\d\d\d\d|\d\d?\. \w{3,10} \d\d\d\d) ?(\(.+\))?$", titlex)
-        if match_date:
-            drafting_date = match_date.group(2)
-        else:
-            drafting_date = "NA"
+    # getting drafting_date from the title. unfortunately, there is no other specific section
+    # where the drafting_date is to be found coherently
+    match_date = re.search(r"^.+(vom|v\.) (\d\d?\.\d\d?\.\d\d\d\d|\d\d?\. \w{3,10} \d\d\d\d) ?(\(.+\))?$", titlex)
+    if match_date:
+        drafting_date = match_date.group(2)
+    else:
+        drafting_date = "NA"
 
-        #getting decade and year
-        if drafting_date == "NA":
-            decade = "NA"
-            year = "NA"
-        else:
-            match_decade = re.search(r"((\d\d\d)\d)$", drafting_date)
-            decade = match_decade.group(2) + "0"
-            year = match_decade.group(1)
+    #getting decade and year
+    if drafting_date == "NA":
+        decade = "NA"
+        year = "NA"
+    else:
+        match_decade = re.search(r"((\d\d\d)\d)$", drafting_date)
+        decade = match_decade.group(2) + "0"
+        year = match_decade.group(1)
 
-        # building the <text> tag
-        text_tag = '<text type="%s" level="%s" title="%s" title_abbreviation="%s" drafting_date="%s" decade="%s" database_URL="%s" court="%s" court_detail="%s" reference="%s" year="%s" decision_type="%s" ECLI="%s">' % (type, level, title, title_abbreviation, drafting_date, decade, database_URL, court, court_detail, reference, year, decision_type, ECLI)
-        corpus_as_list.append(text_tag)
+    # building the <text> tag
+    text_tag = '<text type="%s" level="%s" title="%s" title_abbreviation="%s" drafting_date="%s" decade="%s" database_URL="%s" court="%s" court_detail="%s" reference="%s" year="%s" decision_type="%s" ECLI="%s">' % (type, level, title, title_abbreviation, drafting_date, decade, database_URL, court, court_detail, reference, year, decision_type, ECLI)
+    corpus_as_list.append(text_tag)
 
-        #scraping and adding the text body
-        body = soup.get_text('\n', strip=True)
-        corpus_as_list.append(body)
+    # scraping and adding the text body
+    body = soup.get_text('\n', strip=True)
+    corpus_as_list.append(body)
 
-        #adding the </text> closing tag
-        corpus_as_list.append("</text>")
+    # adding the </text> closing tag
+    corpus_as_list.append("</text>")
 
-        corpus_as_string = "\n".join(corpus_as_list)
-
-        sp.text = "   %i out of 813 (%.2f%%)" % (id, (id/813*100))
+    # printing progress
+    print("\r", "%i out of %i (%.2f%%)" % (id, len(mylist), (id/(len(mylist))*100)), end="")
 
 with open(path_output, "w+", encoding="utf-8") as corpus:
-    corpus.write(corpus_as_string)
+    corpus.write("\n".join(corpus_as_list))
 
 print("\rDone")
